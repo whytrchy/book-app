947cceb78302002bb17baa758638bb9cdabcaf3c
Wahyu <wahyutrchy@rocketmail.com>
2020-09-08 16:04:46 +0700

    first commit

---
+++ build/server/src/config/config.js
@@ -0,0 +1,28 @@
+"use strict";
+
+require('dotenv').config();
+
+module.exports = {
+  "development": {
+    "username": "postgres",
+    "password": "passw0rd",
+    "database": "books",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  },
+  "test": {
+    "username": "postgres",
+    "password": "passw0rd",
+    "database": "book_test",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+};
+//# sourceMappingURL=config.js.map
